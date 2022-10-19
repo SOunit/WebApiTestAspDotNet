64ef5138a2590b94eb677345788b523ad572458a
SOunit <workbookwork7@gmail.com>
2022-10-18 19:15:08 -0700

    Add project files.

---
+++ WebApiTestAspDotNet/ApiControllers/BrandsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WebApiTestAspDotNet.Models;
+
+namespace WebApiTestAspDotNet.ApiControllers
+{
+    public class BrandsController : ApiController
+    {
+        public List<Brand> Get()
+        {
+            var brandsList = new List<Brand>();
+
+            brandsList.Add(new Brand()
+            {
+                Id = 1,
+                Name = "Test1"
+            });
+
+            return brandsList;
+        }
+    }
+}
---
+++ WebApiTestAspDotNet/Models/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiTestAspDotNet.Models
+{
+    public class Brand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
